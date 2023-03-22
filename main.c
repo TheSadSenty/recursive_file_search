dc3c8589096815eebdbb87dd76ff1be149838306
Kasyanov Maxim <maxkasser90@gmail.com>
2023-03-23 01:24:18 +0300

    - Implement recursive string search in file

--- main.c
+++ main.c
@@ -3,73 +3,89 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/types.h>
-#include <sys/param.h>      // for MIN()
+#include <sys/param.h> // for MIN()
 #include <dirent.h>
 #include <memory.h>
-void walk_dir(char *);
+void walk_dir_impl(char *, char *);
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
         printf("Usage: %s <dir>\n\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    
-    walk_dir(argv[1]);
+    walk_dir_impl(argv[1], argv[2]);
 
     return EXIT_SUCCESS;
 }
 
-void print_file(const char *fire_path) {//print entry file
-    if (!strcmp(fire_path, ".") || !strcmp(fire_path, ".."))
+void string_search(const char *file_path, char* search_string)
+{
+    if (!strcmp(file_path, ".") || !strcmp(file_path, ".."))
         return;
 
-    FILE *fp = fopen(fire_path,"r");
-    char buffer[256];
-    if(fp){
-        while (fgets(buffer,256,fp)!=NULL)
+    FILE *fp = fopen(file_path, "r");
+
+    size_t search_string_char_size=sizeof(search_string[0]);
+    size_t len_search_string = strlen(search_string)+1;//because fgets read until n-1
+
+    char *buffer = calloc(len_search_string,search_string_char_size); 
+    //printf("%ld \n", search_string_char_size);
+    //printf("%ld \n", len_search_string);
+    int step=0;
+    if (fp)
+    {
+        while (fgets(buffer, len_search_string, fp) != NULL)
         {
-            printf("%s\n", buffer);
-        }
-        
-    }
-
-    
-}
-
-void walk_dir_impl(int ind_level, char *dir) {
-    DIR *d = opendir(dir);
-    if (d == NULL) {
-        printf("Failed to opendir() %s\n", dir);
-        return;
-    }
-    
-    while(1) {
-        errno = 0;
-        struct dirent *p = readdir(d);
-        if (p == NULL) {
-            if (errno != 0)
-                continue;   // Проблема, переходим к следующему элементу
-            else
-                break;      // В каталоге больше ничего нет
-        }
-
-        if (strcmp(p->d_name, ".") && strcmp(p->d_name, "..")) {
-
-            char file_path[PATH_MAX]={0};//construct path to files
-            sprintf(file_path,"%s/%s",dir,p->d_name);
-            print_contaiment(file_path);
-
-            if (p->d_type == DT_DIR) {
-                char buf[PATH_MAX] = {0};
-                sprintf(buf, "%s/%s", dir, p->d_name);
-                walk_dir_impl(ind_level + 1, buf);
+            //printf("%s|%s \n",file_path,buffer);
+            if(strcmp(buffer, search_string)==0){
+                printf("%s \n", "Find one");
+                fseek(fp,++step,SEEK_SET);
+            }
+            else{
+                fseek(fp,++step,SEEK_SET);
             }
         }
     }
-    
-    closedir(d);
 }
 
-void walk_dir(char *dir) {
-    walk_dir_impl(0, dir);
+void walk_dir_impl(char *dir, char *sequence)
+{
+    DIR *d = opendir(dir);
+    if (d == NULL)
+    {
+        printf("Failed to opendir() %s\n", dir);
+        return;
+    }
+
+    while (1)
+    {
+        errno = 0;
+        struct dirent *p = readdir(d);
+        if (p == NULL)
+        {
+            if (errno != 0)
+                continue; // Проблема, переходим к следующему элементу
+            else
+                break; // В каталоге больше ничего нет
+        }
+
+        if (strcmp(p->d_name, ".") && strcmp(p->d_name, ".."))
+        {
+
+            char file_path[PATH_MAX] = {0}; // construct path to files
+            sprintf(file_path, "%s/%s", dir, p->d_name);
+            string_search(file_path,sequence);
+
+            if (p->d_type == DT_DIR)
+            {
+                char buf[PATH_MAX] = {0};
+                sprintf(buf, "%s/%s", dir, p->d_name);
+                walk_dir_impl(buf, sequence);
+            }
+        }
+    }
+
+    closedir(d);
 }
