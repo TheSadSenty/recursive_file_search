cacf10d0060242b0487aea8c008451ab3347c7fa
Kasyanov Maxim <maxkasser90@gmail.com>
2023-04-30 18:18:08 +0300

    Rewrite validate_plugin.c to check for special plugins

--- validate_plugin.c
+++ validate_plugin.c
@@ -1,4 +1,48 @@
 #include <stdio.h>
-void validate_plugin(char* plugin_path){
-    printf("Find out one at: %s\n", plugin_path);
+#include "plugin_api.h"
+#include <dlfcn.h>
+void validate_plugin(char *plugin_path)
+{
+    // init special struct
+    printf("%s\n", plugin_path);
+    struct plugin_info pi;
+    // try to open .so
+    void *dl = dlopen(plugin_path, RTLD_LAZY);
+    if (!dl)
+    {
+        fprintf(stderr, "ERROR: dlopen() failed: %s\n", dlerror());
+        goto END;
+    }
+    // Check for plugin_get_info() func
+    void *func = dlsym(dl, "plugin_get_info");
+    if (!func)
+    {
+        fprintf(stderr, "ERROR: dlsym() failed: %s\n", dlerror());
+        goto END;
+    }
+    // init plugin_info func
+    typedef int (*pgi_func_t)(struct plugin_info *);
+    pgi_func_t pgi_func = (pgi_func_t)func;
+    int ret = pgi_func(&pi);
+    if (ret < 0)
+    {
+        // if ret!=0 then plugin is not valid
+        fprintf(stderr, "ERROR: plugin_get_info() failed\n");
+        goto END;
+    }
+    // Plugin info
+    fprintf(stdout, "Plugin purpose:\t\t%s\n", pi.plugin_purpose);
+    fprintf(stdout, "Plugin author:\t\t%s\n", pi.plugin_author);
+    fprintf(stdout, "Supported options: ");
+    if (pi.sup_opts_len > 0)
+    {
+        fprintf(stdout, "\n");
+        for (size_t i = 0; i < pi.sup_opts_len; i++)
+        {
+            fprintf(stdout, "\t--%s\t\t%s\n", pi.sup_opts[i].opt.name, pi.sup_opts[i].opt_descr);
+        }
+    }
+END:
+    if (dl)
+        dlclose(dl);
 }
