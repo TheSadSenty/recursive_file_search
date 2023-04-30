b871cbe0b7b50581d13aba59252a482e28634948
Kasyanov Maxim <maxkasser90@gmail.com>
2023-05-01 02:08:38 +0300

    Rewrite argument_parser.c for new purposes

--- argument_parser.c
+++ argument_parser.c
@@ -8,39 +8,36 @@
 #include "functions.h"
 void argument_parser(int argc, char *argv[])
 {
-    static struct option long_options[] = {
-        {"help", no_argument, 0, 0},
-        {"version", no_argument, 0, 1}};
-    int option_index = 0;
-    int choice;
-    while ((choice = getopt_long(argc, argv, "vhP:", long_options, &option_index)) != -1)
+    if ((argc == 1) || ((strncmp(argv[1], "-h", sizeof("-h"))) == 0 && (argc == 2)))
     {
-        switch (choice)
-        {
-        case 0://if choice 0 or h - print help
-
-        case 'h':
-            printf("Usage: %s -P /path/to/*.so [logical operators][plugins parameters]\n", argv[0]);
-            printf("Options:\n");
-            printf("-P\t\tPath to plugins, which implements plugin_api.h interface\n");
-            printf("-A\t\tLogical AND for plugin options\n");
-            printf("-O\t\tLogical OR for plugin options\n");
-            printf("-N\t\tLogical NOT for plugin options, after -A or -O options\n");
-            printf("-h, --help\tPrint help\n");
-            printf("-v, --version\tPrint version\n");
-            printf("Example:\n");
-            printf("%s -O -N --exe --file-len 1000 /tmp\tSearch any files except for executable with length==1000 bytes\n", argv[0]);
-            exit(EXIT_SUCCESS);
-        case 1:
-
-        case 'v'://if choice 1 or v - print version
-            printf("Kasyanov Maxim\nN32451\nVariant 13\n");
-            exit(EXIT_SUCCESS);
-        case 'P':
-            walk_dir_impl(argv[2]);
-            break;
-        case '?':
-            exit(EXIT_FAILURE);
-        }
+        /* Print help */
+        printf("Usage: %s -P /path/to/*.so [logical operators][plugins parameters]\n", argv[0]);
+        printf("Options:\n");
+        printf("-P\t\tPath to plugins, which implements plugin_api.h interface\n");
+        printf("-A\t\tLogical AND for plugin options\n");
+        printf("-O\t\tLogical OR for plugin options\n");
+        printf("-N\t\tLogical NOT for plugin options, after -A or -O options\n");
+        printf("-h, --help\tPrint help\n");
+        printf("-v, --version\tPrint version\n");
+        printf("Example:\n");
+        printf("%s -O -N --exe --file-len 1000 /tmp\tSearch any files except for executable with length==1000 bytes\n", argv[0]);
+        exit(EXIT_SUCCESS);
+    }
+    if ((argc == 2) && strncmp(argv[1], "-v", sizeof("-v")) == 0)
+    {
+        /* Print version */
+        printf("Kasyanov Maxim\nN32451\nVariant 13\n");
+        exit(EXIT_SUCCESS);
+    }
+    if (strncmp(argv[1], "-P", sizeof("-P")) == 0)
+    {
+        /* Start search for .so files in directory */
+        walk_dir_impl(argv[2]);
+    }
+    else
+    {
+        /* Unrecognized options handler */
+        printf("%s: Unrecognized options\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 }
